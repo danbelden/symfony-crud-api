00593b606f884741510249bbeba3a28cae773153
Dan Belden <dan@free-fall.co.uk>
2017-05-11 15:31:40 +0100

    [Initial] Addinging initial symfony 3.2.8 project with docker image

---
+++ web/app_dev.php
@@ -0,0 +1,26 @@
+<?php
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Debug\Debug;
+
+// If you don't want to setup permissions the proper way, just uncomment the following PHP line
+// read http://symfony.com/doc/current/setup.html#checking-symfony-application-configuration-and-setup
+// for more information
+//umask(0000);
+
+// Prevent access unless environment var SYMFONY_ENV set to dev mode
+if (getenv('SYMFONY_ENV') !== 'dev') {
+    header('HTTP/1.0 403 Forbidden');
+    exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
+}
+
+/** @var \Composer\Autoload\ClassLoader $loader */
+$loader = require __DIR__.'/../app/autoload.php';
+Debug::enable();
+
+$kernel = new AppKernel('dev', true);
+$kernel->loadClassCache();
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+$kernel->terminate($request, $response);
